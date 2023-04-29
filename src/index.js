05fbbab46483f6b5b9d6ca8b9e9dc5c29c7c8530
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-04-29 11:21:32 +0200

    work

--- src/index.js
+++ src/index.js
@@ -1,33 +1,113 @@
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
-const submit = document.querySelector('.search-form');
-const input = document.querySelector('searchQuery');
+const form = document.querySelector('.search-form');
+const [inputValue] = document.getElementsByName('searchQuery');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+form.addEventListener('submit', onFormSubmit);
+loadMore.addEventListener('click', onSearchImg);
 
-submit.addEventListener('submit', onFetch);
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '35822629-b9359d31660fe7f3ad5b1d613';
 
-// const axios = require('axios');
-// axios
-//     .get('/user?ID=35822629')
-// // user_id: 35822629 
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
+let page = 1;
 
-function onFetch(event) {
-  event.preventDefault;
-  searchParam = input.value;
-  if (searchParam === '') {
-   Notiflix.Notify.info(
-     'Sorry, there are no images matching your search query. Please try again.'
-   );
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  page = 1;
+  gallery.innerHTML = '';
+  loadMore.classList.add('is-hidden');
+
+  onSearchImg();
+}
+
+async function onSearchImg() {
+  let searchImg = inputValue.value.trim();
+  let per_page = 40;
+
+  const params = {
+    key: API_KEY,
+    q: `${searchImg}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: `${per_page}`,
+    page: `${page}`,
+  };
+
+  try {
+    if (searchImg === '') {
+      Notiflix.Notify.info('No data!');
+      return;
+    }
+    const response = await axios.get(BASE_URL, { params });
+    const photos = await response.data;
+
+    const photosLength = photos.hits.length;
+    if (photosLength === 0) {
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      createMarkup(photos);
+      toggleLoadMoreButton(photosLength);
+      page++;
+      const totalPage = Math.ceil(photos.totalHits / per_page);
+      if (page > totalPage) {
+        Notiflix.Notify.warning(
+          'We are sorry,but you have reached the end of search results.'
+        );
+        loadMore.classList.add('is-hidden');
+        return;
+      }
+    }
+    return photos;
+  } catch (error) {
+    console.log(error.message);
   }
-  fentch('https://pixabay.com/images/search/${searchParam}');
+}
+
+function createMarkup(photos) {
+  const markup = photos.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+      <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width="446" height="250" /></a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${downloads}</b>
+        </p>
+      </div>
+    </div>`;
+      }
+    )
+    .join(' ');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function toggleLoadMoreButton(length) {
+  if (length < 40) {
+    loadMore.classList.add('is-hidden');
+  } else if (loadMore.classList.contains('is-hidden')) {
+    loadMore.classList.remove('is-hidden');
+  }
 }
